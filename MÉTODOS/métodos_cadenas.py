7d4196ef12e8fb1edd5095043d0a75bc6a128820
Eliezer <jose.hoyos2@udea.edu.co>
2025-08-22 21:48:01 -0500

    clase #3

--- MÉTODOS/métodos_cadenas.py
+++ MÉTODOS/métodos_cadenas.py
@@ -1,4 +1,4 @@
-cadena1 = "Hola soy Eliecer"
+cadena1 = "Hola, soy, Eliecer"
 cadena2 = "Bienvenido José"
 
 #convierte a mayusculas
@@ -10,9 +10,71 @@ mayusc = cadena1.upper()
 minusc = cadena1.lower()
 
 #primera letra en mayuscula
-
 primer_letra_mayuscula = cadena1.capitalize()
 
 print(mayusc)
 print(minusc)
 print(primer_letra_mayuscula)
+
+#busqueda de una cadena en otra cadena/ si no hay coincidencias devuelve -1
+
+busqueda_find = cadena1.find("d")
+
+print(busqueda_find)
+
+#busqueda de una cadena en otra cadena
+
+busqueda_index = cadena1.index("H")
+
+print(busqueda_index)
+
+#si es numérico devuelve True, sino False
+
+es_numérico = cadena1.isnumeric()
+
+print(es_numérico)
+
+#si es alfanumérico devuelve True, sino False
+
+es_alfanumérico = cadena1.isalpha()
+print(es_numérico)
+
+#contamos las coincidencias  de una cadena, dentro de otra cadena, devuelve la cantidad de coincidencias.
+
+contar_coincidencia = cadena1.count("a")
+
+print(contar_coincidencia)
+
+#contamos cuántos caracteres tiene una cadena
+
+contar_caracteres = len(cadena1)
+
+print(contar_caracteres)
+
+#verificamos si una cadena empieza con otra cadena dada, si es asi devuelve True
+
+empieza_con = cadena1.startswith("H")
+
+print(empieza_con)
+
+
+#verificamos si una cadena termina con otra cadena dada, si es asi devuelve True
+
+termina_con = cadena1.endswith("H")
+
+print(termina_con)
+
+
+#reemplaza un pedazo de la cadena dada por otra dada
+
+cadena_nueva = cadena1.replace("Hola", "Holu maquina")
+
+print(cadena_nueva)
+
+
+
+#separar cadenas con la cadena que le pasemos
+
+cadena_separada = cadena1.split(",")
+
+print(cadena_separada)
--- VARIABLES/variables.py
+++ VARIABLES/variables.py
@@ -16,3 +16,6 @@
 #BOOLEANOS
 
 True / False
+
+
+
